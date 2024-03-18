12bf5dd6d2f89af518c5c40ca1f6971ef29eb528
Aram Limpens <a.limpens@amsterdam.nl>
2024-03-18 15:07:09 +0100

    Setup linting

--- libs/ui/src/lib/ui.tsx
+++ libs/ui/src/lib/ui.tsx
@@ -1,11 +1,8 @@
 import styles from './ui.module.css';
 
-/* eslint-disable-next-line */
-export interface UiProps {}
-
-export function Ui(props: UiProps) {
+export function Ui() {
   return (
-    <div className={styles['container']}>
+    <div className={styles.container}>
       <h1>Welcome to Ui!</h1>
     </div>
   );
